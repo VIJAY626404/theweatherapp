018d59640705ced4c94b1d03dec2ca72eabd5a35
Vijay Kumar Shah <97323848+VIJAY626404@users.noreply.github.com>
2023-12-14 17:55:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-from flask import Flask , render_template, request
+from flask import Flask , render_template, request,send_file
 import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
-    return render_template("index4.html")
+    return render_template("index.html")
+
 
 @app.route("/weatherapp",methods = ['POST' , "GET"])
 def get_weatherdata():
     url = "https://api.openweathermap.org/data/2.5/weather"
+    datakey = '44b163b4d584716f496867c93190d07e'
 
     param = {
         'q':request.form.get("city"),
-        'appid':request.form.get('appid'),
+        'appid':datakey,
         'units':request.form.get('units')
         }
     response = requests.get(url,params=param)
     data = response.json()
     return f"data : {data}"
+   
+    
 
 if __name__ == '__main__':
     app.run(host= "0.0.0.0" , port = 5002)
